73db8a8c687083c7b075973a407e1f33aac94003
Matthew Wolff <mwolff3@tigers.lsu.edu>
2015-03-04 15:59:11 -0600

    Working instruments
    The keyboard works, also added Tone.js to the repo

--- Web Audio Instruments/hello.js
+++ Web Audio Instruments/hello.js
@@ -16,6 +16,7 @@ function dialChange(input)
 {
 	oscillator.setFrequency((input*220)+220);
 }
+//Tone Transport, matrix
 function keyboardChange(input)
 {
 	if(input.on!=0)
@@ -26,9 +27,11 @@ function keyboardChange(input)
 		//wikipedia.org/MIDI Tuning System
 		var frequency = Math.pow(2,(input.note-69)/12) * 440;
 		console.log(frequency);
-		synth.triggerAttackRelease(frequency,1,0,1);
+		//Having zero time was weird. Glad that got fixed. 
+		synth.triggerAttack(frequency,.25);
 		//synth.envelope = new Tone.Envelope();
 	}
+	else synth.triggerRelease();
 }
 function envChange(input)
 {
@@ -36,9 +39,12 @@ function envChange(input)
 }
 nx.onload = function()
 {
+	console.log("loaded");
 	toggle1.on('value', function(data){toggleChange(data);});
 	dial1.on('value', function(data){dialChange(data);});
 	keyboard1.octaves = 8;
+	//change offset, make it bigger
+	keyboard1.init();
 	keyboard1.on('*', function(data){keyboardChange(data);});
 	envelope1.on('*', function(data){envChange(data);});
 }

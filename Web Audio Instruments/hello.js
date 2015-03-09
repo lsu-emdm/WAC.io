e42d900be0fc9c440d4cb62ff51d322dc123049b
Matthew Wolff <mwolff3@tigers.lsu.edu>
2015-03-09 00:30:16 -0500

    Polyphony
    Polyphony works, some weird quirks.
    
    Also trying to get NXJoints to map to the four voices of the polyphonic
    synth.

--- Web Audio Instruments/hello.js
+++ Web Audio Instruments/hello.js
@@ -1,9 +1,11 @@
-var oscillator = new Tone.Oscillator(220, "square");
+var oscillator = new Tone.Oscillator(220, "sine");
 oscillator.toMaster();
-//var omni = new Tone.OmniOscillator(220,"sawtooth");
-var synth = new Tone.MonoSynth();
+var synth = new Tone.PolySynth(4, Tone.MonoSynth);
 synth.toMaster();
-//var env = new Envelope();
+//Why are underscores so popular?
+synth._voices[1].type = "square";
+synth._voices[2].type = "triangle";
+synth._voices[3].type = "sawtooth";
 function toggleChange(input)
 {
 	switch(input)
@@ -19,32 +21,63 @@ function dialChange(input)
 //Tone Transport, matrix
 function keyboardChange(input)
 {
+	var frequency = Math.pow(2,(input.note-69)/12) * 440;
 	if(input.on!=0)
 	{
-		console.log(input);
-		//need to translate note to frequency
-		//There has to be an easier way...
-		//wikipedia.org/MIDI Tuning System
-		var frequency = Math.pow(2,(input.note-69)/12) * 440;
-		console.log(frequency);
-		//Having zero time was weird. Glad that got fixed. 
-		synth.triggerAttack(frequency,.25);
-		//synth.envelope = new Tone.Envelope();
+		//console.log(input);
+		synth.triggerAttack(frequency,1);
 	}
-	else synth.triggerRelease();
+	else synth.triggerRelease(frequency);
 }
 function envChange(input)
 {
-	console.log(input);
+	//console.log(input);
+	synth.envelope.attack = input.x;
+	synth.envelope.decay = 1 - input.x;
+	synth.envelope.sustain = input.x;
+	synth.envelope.release = 1 - input.x;
+}
+function sliderChange(input)
+{
+	joints1.threshold = input.value * 100;
+	joints1.init();
+}
+function jointsChange(input)
+{
+	if(input.node0)
+	{
+		synth._voices
+	}
+	if(input.node1)
+	{
+		
+	}
+	if(input.node2)
+	{
+		
+	}
+	if(input.node3)
+	{
+		
+	}
 }
 nx.onload = function()
 {
-	console.log("loaded");
+	//console.log("loaded");
 	toggle1.on('value', function(data){toggleChange(data);});
 	dial1.on('value', function(data){dialChange(data);});
-	keyboard1.octaves = 8;
+	//keyboard1.octaves = 8;
 	//change offset, make it bigger
-	keyboard1.init();
+	//keyboard1.init();
 	keyboard1.on('*', function(data){keyboardChange(data);});
 	envelope1.on('*', function(data){envChange(data);});
+	joints1.joints = [
+	{ x: 25, y:25 },
+		{ x:75, y:25},
+			{ x:25, y:75},
+				{ x:75, y:75}
+	]
+	slider1.on('*', function(data){sliderChange(data);});
+	joints1.init();
+	joints1.on('*', function(data){jointsChange(data);});
 }

dcf8ae2eb04626245d91d965702966b340520b5c
Jesse Allison <jesse@electrotap.com>
2015-08-06 16:46:32 -0500

    Simplified server Example

--- server/nexus-server.js
+++ server/nexus-server.js
@@ -1,11 +1,11 @@
 // ************************************************
 
-// Traversal Node Server (Web Audio v.0.2)
+// NEXUS Node Server 
 //				Jesse Allison (2015)
 // 
 //	To Launch:
-//		NODE_ENV=production sudo node traversal-node.js 
-//
+//		NODE_ENV=production sudo node nexus-server.js 
+//		(sudo is required to launch on port 80.)
 
 // ************************************************
 
@@ -13,31 +13,43 @@
 	// Setup web app - using express to serve pages
 var express = require('express'),
 		sio = require('socket.io'),
-		http = require('http'),
-		app = express();
+		http = require('http');
+		
+var app = express();
+		// set up express web application
+	app.configure(function () {
+		app.use(express.static(__dirname + '/public'));
+		app.use(app.router);
+	});
+
+		// FIXME: probably don't need this
+	app.get('/', function (req, res) {
+		res.send('hello');
+	});
 
 	//	OSC Setup for sending (and receiving) OSC (to Max)
-		// oscServer is used for receiving osc messages (from Max)
 var osc = require('node-osc');
-var oscServer = new osc.Server(3333, '127.0.0.1');
+		// oscServer is used for receiving osc messages (from Max)
+var oscServer = new osc.Server(7746, '127.0.0.1');
 oscServer.on("message", function (msg, rinfo) {
-			console.log("OSC message:");
-			console.log(msg);
+			// console.log("OSC message:");
+			// console.log(msg);
 					// Setup messages to receive here //
 	if(msg[0] = "/goToSection") {
-		traversalSection = msg[1];
-		shareSection(traversalSection);
+		currentSection = msg[1];
+		shareSection(currentSection);
 	}
 });
+
 		// oscClient is used to send osc messages (to Max)
 var oscClient = new osc.Client('127.0.0.1', 7745);
 
 
-	// server is the node server (via express)
+	// server is the node server (web app via express)
 		// this code launches the server on port 80 and switches the user id away from sudo
-		// apparently this makes it more secure - if something goes awry it isn't the superuser!
+		// apparently this makes it more secure - if something goes awry it isn't running under the superuser.
 var server = http.createServer(app)
-	.listen(80, function(err) {
+	.listen(8000, function(err) {
 		if (err) return cb(err);
 
 		// Find out which user used sudo through the environment variable
@@ -47,24 +59,15 @@ var server = http.createServer(app)
 		console.log('Server\'s UID is now ' + process.getuid());
 	});
 
-	// FIXME: Can I move this up with app definition?
-app.configure(function () {
-	app.use(express.static(__dirname + '/public'));
-	app.use(app.router);
-});
-
-	// FIXME: probably don't need this
-app.get('/', function (req, res) {
-	res.send('hello');
-});
-
+		// start socket.io listening on the server
 var io = sio.listen(server);
+
 
 // *********************
 	// Global Variables!
 
 var ioClients = [];		// list of clients who have logged in.
-var traversalSection = 0;		// current section.
+var currentSection = 0;		// current section.
 
 // *********************
 
@@ -76,16 +79,18 @@ io.sockets.on('connection', function (socket) {
 		if(username != "admin") {	
 			ioClients.push(socket.id);
 		}
-		socket.username = username;
-		// socket.initials = username.parseInitials;	// Must implement...
+		socket.username = username;  // allows the username to be retrieved anytime the socket is used
+		// Can add any other pertinent details to the socket to be retrieved later
+		// socket.location, etc.
 		var userColor = "#0af";
 		socket.userColor = userColor;
+			// .emit to send message back to caller.
 		socket.emit('chat', 'SERVER', 'You have connected');
-		socket.broadcast.emit('chat', 'SERVER', username + ' is on deck');
+		// .broadcast to send message to all sockets.
+		//socket.broadcast.emit('chat', 'SERVER', username + ' is on deck');
 		socket.emit('bump', socket.username, "::dude::");
-		// sendSection(traversalSection);	 // Sets everyone's section
-		var title = getSection(traversalSection);
-		socket.emit('setSection', traversalSection, title);
+		var title = getSection(currentSection);
+		socket.emit('setSection', currentSection, title);
 	});
 	
 	 socket.on('disconnect', function() {
@@ -98,81 +103,62 @@ io.sockets.on('connection', function (socket) {
 			io.sockets.emit('chat', socket.username, data);
 	 });
 
-	socket.on('bumped', function(data) {
+	socket.on('tap', function(data) {
 		console.log("Data: ", data.inspect);
 		
-		oscClient.send('/bump', 1);
+		oscClient.send('/tapped', 1);
 		
-		socket.broadcast.emit('bump', socket.username, 1);
+		socket.broadcast.emit('tapped', socket.username, 1);
 	});
 	
+	socket.on('shareToggle', function(data) {
+		socket.broadcast.emit('setSharedToggle', data);
+	});
 	
-	socket.on('startTraversal', function(data) {
-		console.log("Start! " + data);
+	socket.on('location', function(data) {
+		if(data) {
+			oscClient.send('/location', data[0], data[1]);
+		}
+	});
+	
+	socket.on('slider', function(data) {
+		console.log("slider! " + data);
 		
-		oscClient.send('/start', data);
+		oscClient.send('/slider', socket.username, data);
 	});
-	socket.on('endSection3', function(data) {
-		console.log("endSection3! " + data);
-		
-		oscClient.send('/endSection3', data);
-	});
-	
-	socket.on('triggerEvent', function(data) {
-		console.log("triggerEvent! " + data);
-		
-		oscClient.send('/triggerEvent', data);
-	});
-	
-	
-	socket.on('popBlit', function(data) {
-		console.log("pop! " + data);
-		
-		oscClient.send('/popBlit', data);
-	});
-	
-	socket.on('noPopBlit', function(x,y) {
-		console.log("noPop! " + x , y, socket.username);
-		var user = getNextUser();
-		user.emit("makeBlit",x,y,socket.username);
-		// oscClient.send('/popBlit', data);
-	});
-	
-	socket.on('explodeBlot', function(data) {
-		console.log("Exploded! " + data);
-		
-		oscClient.send('/exploded', data);
-	});
+
 			
-			// TODO: Add location of user you are connecting with...
-	socket.on('connected', function(data) {
-		console.log("Connected! ");
-		
-		oscClient.send('/connected', socket.username);
-		
+			// Return random user name
+	socket.on('getSomebody', function(data) {
+		console.log("Get Somebody! ");
+				
 		var user = getNextUser();
-					// Make sure you don't connect with yourself
+					// Make sure you don't get yourself
 		if(user.username == socket.username || user.username == null) {
 			user = getNextUser();
 			if(user.username == socket.username || user.username == null) {
-				socket.emit("connectWith","Holden");
+				socket.emit("somebodyToYou","Somebody");
 			}
 		} else {
-			socket.emit("connectWith",user.username);
+			socket.emit("somebodyToYou",user.username);
 		}
 	});
 	
 	
 	socket.on('section', function(data) {
 		console.log("Section: "+ data);
-		traversalSection = data;
-		sendSection(traversalSection);
+		currentSection = data;
+		sendSection(currentSection);
 	})
 
 	// *********************
 			// Functions for handling stuff
 
+			// **** SECTIONS ****
+	var sectionTitles = ["Welcome", "Section 1", "Section 2", "Section 3", "Section 4", "Section 5", "Section 6", "End"];
+	
 	// Todo: Add sections to correspond to organ interactions
+	// sendSection(currentSection);	 // Sets everyone's section
 	sendSection = function (sect) {
 		var title = getSection(sect);
 		
@@ -188,23 +174,14 @@ io.sockets.on('connection', function (socket) {
 	
 	getSection = function(sect) {
 		var title = "none";
-		if(sect == 1) {
-			title = "Hold until burst";
-		} else if (sect == 2) {
-			title = "Tap Blit to Pop, Anywhere to send a Blit";
-		} else if (sect == 3) {
-			title = "Connect with Others";
-		} else if (sect == 4) {
-			title = "Tap Blit to Pop, Anywhere to send a Blit";
-		} else if (sect == 5) {
-			title = "Drag and Connect with Others";
-		} else if (sect == 6) {
-			title = "The End (thank you)";
-		} else if (sect == 0) {
-			title = "<- Device Audio Toggle (Only use with Newer Devices)";
+		if(sectionTitles.length >= sect) {
+			title = sectionTitles[sect];
+		} else {
+			title = "";
 		}
 		return title;
 	}
+		// **** 		****
 	
 				// pick a random user from those still connected and return the user
 	getRandomUser = function() {
